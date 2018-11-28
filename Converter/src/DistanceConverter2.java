46a7764429471c28b3186e447411092db5b9eaac
burke.bradley <burke.bradley@MU-C1-04.mullen.pvt.k12.co.us>
2018-11-28 10:24:52 -0700

    changing distance converter

---
+++ Converter/src/DistanceConverter2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class DistanceConverter2
+	{
+		 String [] distanceUnits = {"feet", "miles", "meters", "kilometers"};
+		 boolean choosingStartingDistance = true;
+		 String startingUnit;
+		 
+		 while(choosingStartingDistance)
+			 {
+				 
+			 }
+	}
